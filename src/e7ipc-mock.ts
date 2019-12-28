b6b1525742c207ae142abc07baa669068e14cc13
Yagi Sumiya <goatship4@yahoo.co.jp>
2019-12-29 03:46:33 +0900

    Implement

---
+++ src/e7ipc-mock.ts
@@ -0,0 +1,32 @@
+import { Handler, Client, Server } from '@yagisumi/e7ipc-types'
+export * from '@yagisumi/e7ipc-types'
+
+export class Mock<Req, Res> implements Server<Req, Res>, Client<Req, Res> {
+  private handler?: Handler<Req, Res>
+
+  invoke(request: Req): Promise<Res> {
+    if (this.handler === undefined) {
+      return Promise.reject(new Error('No handler'))
+    } else {
+      return this.handler({}, request)
+    }
+  }
+
+  handle(listener: Handler<Req, Res>) {
+    if (this.handler !== undefined) {
+      throw new Error('already exist handler')
+    }
+    this.handler = listener
+  }
+
+  handleOnce(listener: Handler<Req, Res>) {
+    this.handle((ev, req) => {
+      this.removeHandler()
+      return listener(ev, req)
+    })
+  }
+
+  removeHandler() {
+    this.handler = undefined
+  }
+}
